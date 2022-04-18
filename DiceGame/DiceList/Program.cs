a381846327ddd5c09ce287049afa9275e8fc76e9
Jerome Laflamme <jeromelaflammep@gmail.com>
2022-04-18 08:43:50 -0400

    DiceList

--- DiceGame/DiceList/Program.cs
+++ DiceGame/DiceList/Program.cs
@@ -4,11 +4,32 @@ using DiceLib;
 
 namespace DiceGame
 {
-    internal class Program
+    internal class PartieB
     {
         static void Main()
         {
-            List<Dice> diceList = new List<Dice>();
+            Dice[] diceArray = new Dice[5];
+            for (int i = 0; i < diceArray.Length; i++)
+            {
+                diceArray[i] = new Dice();
+            }
+
+            foreach (Dice dice in diceArray)
+            {
+                dice.Throw();
+                Console.WriteLine(dice.ToString());
+            }
+
+            /*List<Dice> diceList = new List<Dice>();
+            for (int i = 0; i < 5; i++)
+            {
+                diceList.Add(new Dice());
+            }
+
+            foreach (var dice in diceList)
+            {
+                Console.WriteLine(dice);
+            }*/
         }
     }
 }
