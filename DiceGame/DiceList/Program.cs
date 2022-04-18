b41961394dc605b4c581638d4b405455379def0a
jerome <jeromelaflammep@gmail.com>
2022-04-18 08:40:56 -0400

    DiceList

---
+++ DiceGame/DiceList/Program.cs
@@ -0,0 +1,14 @@
+using System.Runtime.CompilerServices;
+using DiceLib;
+
+
+namespace DiceGame
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Dice> diceList = new List<Dice>();
+        }
+    }
+}
